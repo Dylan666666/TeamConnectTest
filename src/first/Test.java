07805cabe1ba64711dfab15f0a461ebe3e334ef6
Dylan <1439620614@qq.com>
2020-09-19 21:25:49 +0800

    first commit

---
+++ src/first/Test.java
@@ -0,0 +1,11 @@
+package first;
+
+/**
+ * @Author: Mr_OO
+ * @Date: 2020/9/19 21:23
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("this is git-test !!! ");
+    }
+}
